5ad58b0f96d901682073813fe273164df36799db
pfouilloux <7494211+pfouilloux@users.noreply.github.com>
2025-02-15 18:26:55 +0800

    ci(renovate): group minor updates

--- .github/renovate.config.js
+++ .github/renovate.config.js
@@ -3,6 +3,7 @@ module.exports = {
   username: "hibiscus-collective-renovate",
   gitAuthor: "Renovate Bot <bot@renovateapp.com>",
   onboarding: false,
+  requireConfig: "optional",
   platform: "github",
   forkProcessing: "enabled",
   repositories: ["HibiscusCollective/project-template"],
@@ -19,6 +20,21 @@ module.exports = {
       ],
       dependencyDashboardApproval: false,
       minimumReleaseAge: "0 days",
+    },
+    {
+      matchUpdateTypes: [
+        "minor",
+        "patch",
+        "pin",
+        "pinDigest",
+        "digest",
+        "lockFileMaintenance",
+      ],
+      groupName: "minor-updates",
+    },
+    {
+      matchUpdateTypes: ["major"],
+      label: ["Major Release"],
     },
   ],
 };
