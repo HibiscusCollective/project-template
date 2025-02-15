aa587c039cf2e0a95664eea1da7e36d84be13b3c
pfouilloux <7494211+pfouilloux@users.noreply.github.com>
2025-02-15 17:34:39 +0800

    ci(renovate): remove the dry-run flag

--- .github/renovate.config.js
+++ .github/renovate.config.js
@@ -5,7 +5,6 @@ module.exports = {
   onboarding: false,
   platform: "github",
   forkProcessing: "enabled",
-  dryRun: "full",
   repositories: ["HibiscusCollective/project-template"],
   packageRules: [
     {
