9f3506b8c273412f8da589f3e4aa2618304fa3f1
Pierre Fouilloux <7494211+pfouilloux@users.noreply.github.com>
2025-03-18 22:26:50 +0800

    feat: update linters and formatters

--- .github/renovate.config.js
+++ .github/renovate.config.js
@@ -1,42 +1,28 @@
-module.exports = {
-  branchPrefix: "renovate-bot/",
-  username: "hibiscus-collective-renovate",
-  gitAuthor: "Renovate Bot <bot@renovateapp.com>",
+export default {
+  branchPrefix: 'renovate-bot/',
+  username: 'hibiscus-collective-renovate',
+  gitAuthor: 'Renovate Bot <bot@renovateapp.com>',
   onboarding: false,
-  requireConfig: "optional",
-  platform: "github",
-  forkProcessing: "enabled",
-  repositories: ["HibiscusCollective/project-template"],
+  requireConfig: 'optional',
+  platform: 'github',
+  forkProcessing: 'enabled',
+  repositories: ['HibiscusCollective/project-template'],
   packageRules: [
     {
-      description: "lockFileMaintenance",
-      matchUpdateTypes: [
-        "pin",
-        "digest",
-        "patch",
-        "minor",
-        "major",
-        "lockFileMaintenance",
-      ],
+      description: 'lockFileMaintenance',
+      matchUpdateTypes: ['pin', 'digest', 'patch', 'minor', 'major', 'lockFileMaintenance'],
       dependencyDashboardApproval: false,
-      minimumReleaseAge: "7 days",
+      minimumReleaseAge: '7 days'
     },
     {
-      matchUpdateTypes: [
-        "minor",
-        "patch",
-        "pin",
-        "pinDigest",
-        "digest",
-        "lockFileMaintenance",
-      ],
-      groupName: "minor-updates",
-      minimumReleaseAge: "7 days",
+      matchUpdateTypes: ['minor', 'patch', 'pin', 'pinDigest', 'digest', 'lockFileMaintenance'],
+      groupName: 'minor-updates',
+      minimumReleaseAge: '7 days'
     },
     {
-      matchUpdateTypes: ["major"],
-      labels: ["Major Release"],
-      minimumReleaseAge: "7 days",
-    },
-  ],
-};
+      matchUpdateTypes: ['major'],
+      labels: ['Major Release'],
+      minimumReleaseAge: '7 days'
+    }
+  ]
+}
